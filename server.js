ccf943f8562663b1d9cfc812a75b22884ddbce61
markjanes047-glitch <markjanes047@gmail.com>
2026-09-11 05:12:50 -0700

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const crypto = require("crypto");
 const app = express();
 
 app.use(cors({
-  origin: "https://manfront.onrender.com"
+  origin: "https://ecocash-loan-zusr.onrender.com"
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
